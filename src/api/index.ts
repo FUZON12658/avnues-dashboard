b22f73b2daedeabb1de255cfc986ac3576d4253c
fuzon12658 <fuzon12658@gmail.com>
2025-06-20 21:37:15 +0545

    patch

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,20 @@
 const { default: axios } = require("axios");
 
-const apiHost = process.env.NEXT_PUBLIC_API_HOST 
-console.log(apiHost);
+const apiHost = process.env.NEXT_PUBLIC_API_HOST;
+console.log('Environment variables:', {
+  NEXT_PUBLIC_API_HOST: process.env.NEXT_PUBLIC_API_HOST,
+  NODE_ENV: process.env.NODE_ENV,
+  apiHost: apiHost
+});
+
 export const crAxios = axios.create({
   baseURL: apiHost,
   withCredentials: true,
+});
+
+// Add request interceptor to log actual URLs being called
+crAxios.interceptors.request.use((request:any) => {
+  console.log('Making request to:', request.url);
+  console.log('Full URL:', `${request.baseURL}${request.url}`);
+  return request;
 });
