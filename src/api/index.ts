0309ad97b48c4f644ffbd7fd68ac84ca540c7a6f
fuzon12658 <fuzon12658@gmail.com>
2025-06-20 21:11:14 +0545

    patch

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,8 @@
 const { default: axios } = require("axios");
 
+const apiHost = process.env.NEXT_PUBLIC_API_HOST || 'https://avnews.sayathari.tech';
+
 export const crAxios = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_HOST,
+  baseURL: apiHost,
   withCredentials: true,
 });
